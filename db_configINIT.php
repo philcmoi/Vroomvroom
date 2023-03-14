a5cc5a4b21a4f0b3f3e216ccbc4e710222fa03ba
philcmoi <68182573+philcmoi@users.noreply.github.com>
2023-03-14 17:55:56 +0100

    Add files via upload

--- db_configINIT.php
+++ db_configINIT.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost = "mysql"; // host name
-$dbuser = "u909244959_philippe";  // username
+$dbhost = "localhost"; // host name
+$dbuser = "u909244959_lhpp";  // username
 $dbpass = "l@99339RWFH5465";  // password
-$db = "u909244959_wroomwroom";  // database name
+$db = "u909244959_lhpp";  // database name
 $con = mysqli_connect($dbhost, $dbuser, $dbpass , $db) or die($con);
 // 	mysqli_set_charset($con, "utf8");
 
